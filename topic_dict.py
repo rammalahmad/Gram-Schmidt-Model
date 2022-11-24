f086685bae708fc5fd5d6828f3485651ca560566
rammalahmad <76946348+rammalahmad@users.noreply.github.com>
2022-11-24 20:01:10 +0100

    Add files via upload

---
+++ topic_dict.py
@@ -0,0 +1,8 @@
+""" This code contains the collected dictionnary that is relevant to the environment"""
+
+topic_name = "environment"
+topic_dict = ["Product Stewardship", "Climate Strategy", "Recycle", "Energy Management", "Waste and Hazardous Materials Management", 
+"Water and Wastewater Management", "Emission reduction", "Component reduction", "Renewable energy", "Supplier clean energy", "Product energy efficiency",
+ "Product Design", "Facilities", "energy efficiency", "Materials", "Zero Waste", "environmental friendly Chemistry", "Innovation", "Low carbon design", 
+ "Climate change", "Emission Reduction", "Consumption Reduction", "Development", "Distribution", "Repair", "Use", "Manufacturing", "EOL (Products End of Life)", 
+"Transportation", "Carbon footprint"]
